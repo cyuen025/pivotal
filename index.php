3c8285a1974d14978d3c3e2f198df9d5f77c3ae6
cyuen025 <cyuen025@gmail.com>
2014-10-18 16:27:21 -0700

    cleaned up index.php

--- index.php
+++ index.php
@@ -6,15 +6,13 @@
 <main>
 <div class="inner_wrapper content">
 	<div class="outer_div">
-		<h1 class="animated fadeInRightBig">YOUR NEXT HIRE AWAITS!</h1>
-	</div>
+		<h1 class="animated fadeInRightBig">Your Next Hire Awaits!</h1>
+	</div><!-- .outer_div -->
     <div class="inner_div">
         <div class="htp_content">
-
-           <a href="programs.php">
-            <h3><span class="icon-user"></span></h3>
-
-            <h2>EMPLOYERS</h2>
+            <a href="programs.php">
+                <h3><span class="icon-user"></span></h3>
+                <h2>Employers</h2>
             </a>
 
             <p>At BCIT's High-Tech Professional programs, we take pride in producing the type of graduate that
@@ -22,32 +20,29 @@
             outlines two major concepts: our work-term and IT projects portion of the programs as well as our
             graduates, who are ready, willing and able to begin working for you in a myriad of different
             capacities.</p>
+            
             <p>BCIT's reputation for producing job-ready graduates continues at HTP with over 150 employers
             either hiring full-time or work-term students in addition to our graduates who have completed some
             innovative IT projects.</p>
-        </div><!-- end htp_content -->
+        </div><!-- .htp_content -->
         <div class="htp_content">
-
-           <a href="programs.php">
-            <span class="icon-briefcase"></span>
-
-            <h2>WORK-TERMS</h2>
+            <a href="programs.php">
+                <span class="icon-briefcase"></span>
+                <h2>Work-Terms</h2>
             </a>
-
+            
             <p>We have work-terms in three programs: NASP, OAT and TSP. Work-terms allow students to gain
             valuable hands-on experience onsite, in addition to alleviating your staff shortages, etc. Continuing
             reading for details about the work-term and for the documents and links to secure a position with
             any of the three programs.</p>
-        </div><!-- end htp_content -->
+        </div><!-- .htp_content -->
         <div class="htp_content">
-
             <a href="projects.php">
-            <span class="icon-screen"></span>
-
-            <h2 class="desktop">PROJECTS / WEBSITES</h2>
-            <h2 class="tablet">PROJECTS</h2>
+                <span class="icon-screen"></span>
+                <h2 class="desktop">Projects / Websites</h2>
+                <h2 class="tablet">Projects</h2>
             </a>
-            <div>
+            
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non pellentesque ante, non
             placerat metus. Morbi eget massa at ipsum auctor pretium. Suspendisse tincidunt tincidunt nisi, non
             hendrerit lacus pulvinar vel. Maecenas hendrerit interdum quam sit amet interdum. Cras non iaculis
@@ -55,77 +50,73 @@
             sit amet dui interdum, nec rhoncus dolor consequat. Nullam consectetur blandit sem. Mauris
             vulputate at nisi nec bibendum. Nunc elit felis, ornare vel nisl non, sodales porta elit. Fusce viverra
             arcu eu volutpat iaculis. Mauris ornare lobortis nulla, ac adipiscing augue.</p>
-            </div>
-        </div><!-- end htp_content -->
-
+        </div><!-- .htp_content -->
         <div class="htp_content_tablet">
-
-           <a href="programs.php">
-            <span class="icon-briefcase"></span>
-
-            <h2>WORK-TERMS</h2>
+            <a href="programs.php">
+                <span class="icon-briefcase"></span>
+                <h2>Work-Terms</h2>
             </a>
-
+            
             <p>We have work-terms in three programs: NASP, OAT and TSP. Work-terms allow students to gain
             valuable hands-on experience onsite, in addition to alleviating your staff shortages, etc. Continuing
             reading for details about the work-term and for the documents and links to secure a position with
             any of the three programs.</p>
-        </div><!-- end htp_content -->
-    </div>
+        </div><!-- .htp_content -->
+    </div><!-- .inner_div -->
     <nav class="sub_nav">
         <ul>
             <li class="flip-container" ontouchstart="this.classList.toggle('hover');">
                 <a href="nasp.php">
                     <div class="flipper">
                         <div class="front">
-                        <h2>NASP</h2><br><hr><p class="nav_desktop">Network<br>Administration <br>&#47; Security<br>Professional</p>
-                        <p class="nav_tablet">Network<br>Admin <br>&#47; Security<br>Professional</p><hr>
-                        </div>
+                            <h2>NASP</h2><br><hr><p class="nav_desktop">Network<br>Administration <br>&#47; Security<br>Professional</p>
+                            <p class="nav_tablet">Network<br>Admin <br>&#47; Security<br>Professional</p><hr>
+                        </div><!-- .front -->
                         <div class="back">
-                                <h3>Positions</h3> 
-                                <ul>
-                                    <li>Network Administration</li>
-                                    <li>Systems Support</li>
-                                    <li>Systems Troubleshooting</li>
-                                    <li>Systems Rollout/Upgrade</li>
-                                    <li>Helpdesk</li>
-                                    <li>Technical Writing</li>
-                                    <li>IT Consulting</li>
-                                    <li>IT Training</li>
-                                </ul>
-                        </div>
-                    </div>
+                            <h3>Positions</h3> 
+                            <ul>
+                                <li>Network Administration</li>
+                                <li>Systems Support</li>
+                                <li>Systems Troubleshooting</li>
+                                <li>Systems Rollout/Upgrade</li>
+                                <li>Helpdesk</li>
+                                <li>Technical Writing</li>
+                                <li>IT Consulting</li>
+                                <li>IT Training</li>
+                            </ul>
+                        </div><!-- .back -->
+                    </div><!-- .flipper -->
                 </a>
-            </li>
+            </li><!-- .flip-container -->
             <li class="flip-container">
                 <a href="oat.php">
                     <div class="flipper">
                         <div class="front">
-                        <h2>OAT</h2><br><hr><p class="nav_desktop">Office<br>Administration with<br>Technology</p>
-                        <p class="nav_tablet">Office<br>Admin <br>with<br>Technology</p><hr>
-                        </div>
+                            <h2>OAT</h2><br><hr><p class="nav_desktop">Office<br>Administration with<br>Technology</p>
+                            <p class="nav_tablet">Office<br>Admin <br>with<br>Technology</p><hr>
+                        </div><!-- .front -->
                         <div class="back">
-                            	<h3>Positions</h3>       
-                                <ul>
-                                    <li>Office Manager</li>
-                                    <li>Executive Assistants</li>
-                                    <li>Administrative Assistants</li>
-                                    <li>MS Office Applications Support</li>
-                                    <li>Desktop Publishers/Web Site Maintenance</li>
-                                </ul>
-                        </div>
-                    </div>
+                            <h3>Positions</h3>       
+                            <ul>
+                                <li>Office Manager</li>
+                                <li>Executive Assistants</li>
+                                <li>Administrative Assistants</li>
+                                <li>MS Office Applications Support</li>
+                                <li>Desktop Publishers/Web Site Maintenance</li>
+                            </ul>
+                        </div><!-- .back -->
+                    </div><!-- .flipper -->
                 </a>
-            </li>
+            </li><!-- .flip-container -->
             <li class="flip-container">
                 <a href="ssd.php">
                     <div class="flipper">
-                    <div class="front">
-                    <h2>SSD</h2><br><hr><p>Software<br>Systems<br>Developer</p><hr>
-                    </div>
-                    <div class="back">
+                        <div class="front">
+                            <h2>SSD</h2><br><hr><p>Software<br>Systems<br>Developer</p><hr>
+                        </div><!-- .front -->
+                        <div class="back">
                             <h3>Positions</h3>
-                            
+
                             <ul>
                                 <li>Software Developer</li>
                                 <li>Web Developer</li>
@@ -135,19 +126,19 @@
                                 <li>Web Master</li>
                                 <li>Web Application Developer</li>
                             </ul>
-                    </div>
-                    </div>
+                        </div><!-- .back -->
+                    </div><!-- .flipper -->
                 </a>
-            </li>
+            </li><!-- .flip-container -->
             <li class="flip-container">
                 <a href="twd.php">
                     <div class="flipper">
-                    <div class="front">
-                    <h2>TWD</h2><br><hr><p>Technical<br>Web<br>Designer</p><hr>
-                    </div>
-                    <div class="back">
+                        <div class="front">
+                            <h2>TWD</h2><br><hr><p>Technical<br>Web<br>Designer</p><hr>
+                        </div><!-- .front -->
+                        <div class="back">
                             <h3>Positions</h3>
-                            
+
                             <ul>
                                 <li>Web Designer</li>
                                 <li>Web Developer</li>
@@ -158,33 +149,33 @@
                                 <li>Web Application Designer</li>
                                 <li>eLearning Web Developer</li>
                             </ul>
-                    </div>
-                    </div>
+                        </div><!-- .back -->
+                    </div><!-- .flipper -->
                 </a>
-            </li>
+            </li><!-- .flip-container -->
             <li class="flip-container">
-            <a href="tsp.php">
-                <div class="flipper">
-                <div class="front">
-                <h2>TSP</h2><br><hr><p>Technology<br>Support<br>Professional</p><hr>
-                </div>
-                <div class="back">
-                    	<h3>Positions</h3>
-                        
-                        <ul>
-                            <li>Helpdesk</li>
-                            <li>Tier 1/2 Support</li>
-                            <li>Systems Support</li>
-                            <li>Network Administration</li>
-                            <li>Systems Troubleshooting</li>
-                            <li>Systems Rollout/Upgrade</li>
-                        </ul>
-                </div>
-                </div>
-            </a>
-            </li>
+                <a href="tsp.php">
+                    <div class="flipper">
+                        <div class="front">
+                            <h2>TSP</h2><br><hr><p>Technology<br>Support<br>Professional</p><hr>
+                        </div><!-- .front -->
+                        <div class="back">
+                            <h3>Positions</h3>
+
+                            <ul>
+                                <li>Helpdesk</li>
+                                <li>Tier 1/2 Support</li>
+                                <li>Systems Support</li>
+                                <li>Network Administration</li>
+                                <li>Systems Troubleshooting</li>
+                                <li>Systems Rollout/Upgrade</li>
+                            </ul>
+                        </div><!-- .back -->
+                    </div><!-- .flipper -->
+                </a>
+            </li><!-- .flip-container -->
         </ul>
-    </nav>
+    </nav><!-- .sub_nav -->
     <div class="clear"></div>
     <div class="mobile_sub_nav_spacer"></div>
     
